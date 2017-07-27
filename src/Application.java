c20bc33acc9157d863dfb43a84d9e0764a57f3a6
CHAITU SAINI <CHAITU SAINI@CHAITU>
2017-07-27 11:57:16 -0700

    If Condition

---
+++ src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myint = 15;
+
+		if (myint < 10) {
+			System.out.println("Yes its true");
+		} else if (myint > 20) {
+			System.out.println("No its false");
+		} else {
+			System.out.println("None of the Above");
+		}
+	}
+
+}
